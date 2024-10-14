55cd66c821202df2286c87e29b4ff33caf977780
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-10-14 14:43:07 +0530

    Tiny change

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="Your Api Key for TinyMCE"
+            apiKey="gkjr0thdcotjxpjhfprjdg6iyncc0tsinwi2oerkt1gzf6gm"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
